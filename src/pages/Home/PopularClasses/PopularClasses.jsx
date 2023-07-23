eaf361c3b620e66bea8838962f2a379a9f9450a4
rabby6530 <you@example.com>
2023-07-23 12:04:49 +0600

    temporary done project for assaignment

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import UseClasses from "../../../Hooks/UseClasses";
 
 
@@ -16,28 +17,27 @@ const PopularClasses = () => {
         <div >
 
             <h2 className="text-6xl text-bold text-center mb-8 uppercase">Popular Class</h2>
-           <div className="grid lg:grid-cols-3">
-           {
-                popularClasses.map((popularClass) => <div
-                    key={popularClass?._id}
-                >
-                    <div className="card w-96 bg-base-100 shadow-xl dark:shadow-slate-50  dark:text-gray-100 dark:bg-slate-900">
-                        <figure><img src={popularClass?.image} alt="Shoes" /></figure>
-                        <div className="card-body ">
-                            <h2 className="card-title">{name}</h2>
-                            <p> Price:{popularClass?.price}</p>
-                            <p> Available_seats:{popularClass?.available_seats}</p>
-                            <div className="divider"></div>
-                            <h2 className="text-2xl">Instructor: {popularClass?.instructor_name}</h2>
-                            <p>Email:{popularClass?.instructor_email}</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn">Select Class</button>
+            <div className="grid lg:grid-cols-3">
+                {
+                    popularClasses.map((popularClass) => <div
+                        key={popularClass?._id}
+                    >
+                        <div className="card w-96 bg-base-100 shadow-xl dark:shadow-slate-50  dark:text-gray-100 dark:bg-slate-900">
+                            <figure><img src={popularClass?.image} alt="Shoes" /></figure>
+                            <div className="card-body ">
+                                <h2 className="card-title">{popularClass?.name}</h2>
+                                <h2 className="text-2xl">Instructor: {popularClass?.instructor_name}</h2>
+
+                                <div className="card-actions justify-end">
+                                    <Link to='/classes'>
+                                        <button className="btn">Select Class</button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>)
-            }
-           </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
